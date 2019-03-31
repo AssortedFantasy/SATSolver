de5d5fbcadccd85a09ea93a76de31547287f8b7e
Jehanzeb Mirza <jehanzeb.mirza@yahoo.com>
2019-03-30 22:57:06 -0600

    Expressions defined at a 100 Foot view

--- include/expression.h
+++ include/expression.h
@@ -1,13 +1,24 @@
 #ifndef _EXPRESSION_H
 #define _EXPRESSION_H
 
+#include <vector>
+
+// TODO Move to own .h file?
+// Flags
+#define LIT_VAL (1<<0);
+#define NEGATED (1<<1);
+#define DUAL	(1<<2);
+#define COMPL	(1<<3);
+
+
 /*
 	Expressions are our main class through which everything interacts
 */
-class expression {
-
-
-
+class expression{
+private:
+	int uuid;
+	int FLAGS;
+	std::vector<expression> contents;
 };
 
 
--- src/satsolver.cpp
+++ src/satsolver.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-#include <daogetrw.h>
+#include "expression.h"
+
 
 /*
 	Command Line Interface implementation for the project
 	uses getopt() from 
 */
 int main(int argc, char* argv[]){
+	std::cout << sizeof(long) << "\n" << sizeof(int) << "\n" << sizeof(long long) << "\n" << sizeof(short) << "\n" << sizeof(expression) << "\n";
+	std::cin.get();
 	return 0;
 }
